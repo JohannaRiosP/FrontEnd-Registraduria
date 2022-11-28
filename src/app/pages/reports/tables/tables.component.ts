384f9ae6895292f8497f7570ee567df5a9afba3e
JohannaRiosP <Johannarios.es@gmail.com>
2022-11-27 23:03:56 -0500

    Actualizacion reportes

--- src/app/pages/reports/tables/tables.component.ts
+++ src/app/pages/reports/tables/tables.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Table } from '../../../models/table.model';
+import { TablesService } from '../../../services/tables.service';
+import { ReportsService } from '../../../services/reports.service';
 
 @Component({
   selector: 'ngx-tables',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablesComponent implements OnInit {
 
-  constructor() { }
+  dataTables: Object;
+  dataTable: Object = {
+    tableNumber: "",
+    peopleRegistered: "",
+    votes: "",
+  }
+  tables: Table[];
+  tableId: string = "";
+  totalMode: boolean = true; // true=all false=one
+
+  constructor(private reportsService: ReportsService,
+    private tablesService: TablesService) { }
 
   ngOnInit(): void {
+    this.getTables();
+    if(this.totalMode)
+      this.getDataFull();
   }
 
+  getTables(): void{
+    this.tablesService.list().subscribe(
+      data => {
+        this.tables = data;
+      },
+      error => {
+        console.log(error)
+      }
+    );
+  }
+
+  getDataFull(): void {
+    this.totalMode = true
+    this.reportsService.tablesReport().subscribe(
+      data => {
+        this.dataTables = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getDataOne(): void {
+    this.totalMode = false
+    this.reportsService.tablesReport().subscribe(
+      data => {
+        this.dataTables = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  change(): void{
+    if(this.tableId!="")
+      this.getDataOne();
+    else
+      this.getDataFull();
+      this.ngOnInit();
+    }
 }
